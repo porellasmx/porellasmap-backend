b0072123c6e4f03e00ac1ed6ef0e62dcf99b7c0f
Patricio Vargas <pevargas2730@gmail.com>
2019-09-02 19:24:04 -0400

    added CORS

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-//Middleware
+//CORS
 app.use((req, res, next) => {
-  console.log('my first middleware');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', '*');
   next();
+});
+
+//Middleware
+app.use('/api/reports', (req, res, next) => {
+  res.status(200).json({
+    message: 'Reports fetched successfully',
+    reports: reports
+  });
 });
 
 app.use((req, res, next) => {
