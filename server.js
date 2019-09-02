95f50e0ab8f50ddb7ae3578b252909adeae1ea34
Patricio Vargas <pevargas2730@gmail.com>
2019-09-02 18:59:22 -0400

    udpated server.js

--- server.js
+++ server.js
@@ -1,8 +1,53 @@
-const http = require('http');
 const app = require('./app');
+const debug = require('debug')('node-angular');
+const http = require('http');
 
-const port = process.env.PORT || 8080;
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+//If theres an error with the PORT
+const onError = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
+  console.log('Listening on ' + bind);
+};
+
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
-http.createServer(app).listen(port);
 
-console.log('Server running at http://127.0.0.1:8080/');
+const server = http.createServer(app);
+server.on('error', onError);
+server.on('listening', onListening);
+server.listen(port);
