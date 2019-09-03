0d3782bfc1b98978193a374b42a174558145f92a
Patricio Vargas <pevargas2730@gmail.com>
2019-09-03 03:14:52 -0400

    saving first report

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Report = require('./models/report');
 const app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://<username>:<password>@porellasmap-cluster-tgcab.mongodb.net/porellasmap',
+    { useCreateIndex: true, useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch(() => {
+    console.log('Connection to DB Failed');
+  });
 
 //BODY PARSER
 app.use(bodyParser.json());
@@ -19,16 +33,29 @@ app.use((req, res, next) => {
 
 //Middleware
 
+//Creating a new Report
 app.post('/api/reports', (req, res, next) => {
-  const reports = req.body;
-  console.log(reports);
+  const report = new Report({
+    address: req.body.address,
+    placeName: req.body.placeName,
+    description: req.body.description,
+    abuseType: req.body.abuseType,
+    dateOfEvent: req.body.dateOfEvent,
+    image: req.body.image,
+    lat: req.body.lat,
+    long: req.body.long
+  });
+  console.log(report);
+
+  report.save();
+
   res.status(201).json({
     message: 'Report created successfully',
     report: report
   });
 });
 
-app.use('/api/reports', (req, res, next) => {
+app.get('/api/reports', (req, res, next) => {
   res.status(200).json({
     message: 'Reports fetched successfully',
     reports: reports
