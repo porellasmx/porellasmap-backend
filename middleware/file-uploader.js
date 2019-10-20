34092d92114acd0136e81c5401de7e91dd552c22
Patricio Vargas <pevargas2730@gmail.com>
2019-10-20 02:08:54 -0400

    added multer s3 file uploader

--- middleware/file-uploader.js
+++ middleware/file-uploader.js
@@ -1,19 +1,31 @@
 const aws = require('aws-sdk');
-const express = require('express');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-const s3 = new aws.S3({});
+aws.config.update({
+  secretAccessKey: 'yULR3hPKLlackyu35TiHbHQugz5WMdZ2x/QgWHfX',
+  accessKeyId: 'AKIAJEIXOCFWNXFW5QPA',
+  region: 'us-east-1'
+});
+
+const s3 = new aws.S3();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
+  }
+};
 
 const upload = multer({
   storage: multerS3({
-    s3: s3,
-    bucket: 'some-bucket',
-    metadata: (req, file, cb) => {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: (req, file, cb) => {
-      cb(null, Date.now().toString());
+    acl: 'public-read',
+    s3,
+    bucket: 'porellas',
+    key: function(req, file, cb) {
+      req.file = file;
+      cb(null, file.originalname + Date.now());
     }
   })
 });
